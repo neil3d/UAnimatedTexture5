8d92e7cd8d4b0370f56446cf3ea0476123c4e9ee
neil3d <neil3d@126.com>
2021-12-11 12:23:09 +0800

    reimport handler

--- Plugins/AnimatedTexturePlugin/Source/AnimatedTextureEditor/Public/AnimatedTextureFactory.h
+++ Plugins/AnimatedTexturePlugin/Source/AnimatedTextureEditor/Public/AnimatedTextureFactory.h
@@ -12,6 +12,7 @@
 
 #include "CoreMinimal.h"
 #include "Factories/Factory.h"
+#include "EditorReimportHandler.h"	// UnrealEd
 #include "AnimatedTextureFactory.generated.h"
 
 /**
@@ -19,7 +20,7 @@
  * @see class UTextureFactory
  */
 UCLASS()
-class ANIMATEDTEXTUREEDITOR_API UAnimatedTextureFactory : public UFactory
+class ANIMATEDTEXTUREEDITOR_API UAnimatedTextureFactory : public UFactory, public FReimportHandler
 {
 	GENERATED_UCLASS_BODY()
 
@@ -28,4 +29,11 @@ public:
 	virtual bool FactoryCanImport(const FString& Filename) override;
 	virtual UObject* FactoryCreateBinary(UClass* InClass, UObject* InParent, FName InName, EObjectFlags Flags, UObject* Context, const TCHAR* Type, const uint8*& Buffer, const uint8* BufferEnd, FFeedbackContext* Warn) override;
 	//~ End UFactory Interface
+
+	//~ Begin FReimportHandler Interface
+	virtual bool CanReimport(UObject* Obj, TArray<FString>& OutFilenames) override;
+	virtual void SetReimportPaths(UObject* Obj, const TArray<FString>& NewReimportPaths) override;
+	virtual EReimportResult::Type Reimport(UObject* Obj) override;
+	virtual int32 GetPriority() const override;
+	//~ End FReimportHandler Interface
 };
