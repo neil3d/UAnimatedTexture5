1aff2f6dab02238fcaf36b3ab89f7a9b1522fee4
neil3d <neil3d@126.com>
2021-12-11 12:18:29 +0800

    thumbnail renderer

--- Plugins/AnimatedTexturePlugin/Source/AnimatedTextureEditor/Public/AnimatedTextureThumbnailRenderer.h
+++ Plugins/AnimatedTexturePlugin/Source/AnimatedTextureEditor/Public/AnimatedTextureThumbnailRenderer.h
@@ -14,4 +14,9 @@ class ANIMATEDTEXTUREEDITOR_API UAnimatedTextureThumbnailRenderer : public UThum
 {
 	GENERATED_BODY()
 	
+public:
+	// Begin UThumbnailRenderer Object
+	virtual void GetThumbnailSize(UObject* Object, float Zoom, uint32& OutWidth, uint32& OutHeight) const override;
+	virtual void Draw(UObject* Object, int32 X, int32 Y, uint32 Width, uint32 Height, FRenderTarget* Viewport, FCanvas* Canvas, bool bAdditionalViewFamily) override;
+	// End UThumbnailRenderer Object
 };
